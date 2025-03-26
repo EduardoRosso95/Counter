0d2a530b0c52ad64063dd2ffb7af4d0e72c3cfbf
EduardoRosso95 <eduardo.rossoo@gmail.com>
2025-03-26 23:11:31 +0100

    Modify html and js file, added DOM manipulation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,68 @@
 
-//dichiarazione variabili, tramite i selettori
+//Dichiarazione variabili, tramite i selettori
+const displaycounter_container = document.querySelector("#displaycounter-container");
+const bodycounter_container = document.querySelector("#bodycounter-container");
+const footer = document.querySelector("footer");
 
-const decrementbutton = document.querySelector("#btndecrement");
-const incrementbutton = document.querySelector("#btnincrement");
-const personagesnail = document.querySelector("#personagesnail");
-const displaycounter = document.querySelector("#displaycounter");
+//CREAZIONE ELEMENTI HTML
+//Creo elementi html attraverso la manipolazione del DOM
+//Contatore
+const displaycounter = document.createElement("p");
+const decrementbutton = document.createElement("button");
+const incrementbutton = document.createElement("button");
+const decrementbutton_image = document.createElement("img");
+const incrementbutton_image = document.createElement("img");
+//Personaggio
+const personagesnail = document.createElement("img");
 
+//POSIZIONAMENTO DEGLI ELEMENTI CREATI
+//Posiziono all'interno del display del contatore
+displaycounter_container.appendChild(displaycounter);
+//Posiziono all'interno del corpo del contatore
+bodycounter_container.appendChild(decrementbutton);
+bodycounter_container.appendChild(incrementbutton);
+decrementbutton.appendChild(decrementbutton_image);
+incrementbutton.appendChild(incrementbutton_image);
+//Posiziono il personaggio all'interno del footer
+footer.appendChild(personagesnail);
 
-//Definisco le direzione del personaggio caricando due immagini a specchio*/
+//MODIFICHE DEGLI ELEMENTI CREATI
+//display del contatore
+displaycounter.classList.add("display-1", "fw-bold");
+displaycounter.textContent = 0;
+//corpo del contatore
+//Bottone decremento
+decrementbutton.classList.add("btn", "btn-lg", "px-2");
+decrementbutton_image.src = "./assets/img/btn_decrement_counter.gif";
+decrementbutton_image.classList.add("img-btn-counter");
+decrementbutton_image.alt = "Decrement button";
+decrementbutton_image.width = 100;
+decrementbutton_image.height = 100;
+//Bottone incremento
+incrementbutton.classList.add("btn", "btn-lg", "px-2");
+incrementbutton_image.src = "./assets/img/btn_increment_counter.gif";
+incrementbutton_image.classList.add("img-btn-counter");
+incrementbutton_image.alt = "Increment button";
+incrementbutton_image.width = 100;
+incrementbutton_image.height = 100;
+//Personaggio
+personagesnail.classList.add("img-footer", "pb-5");
+personagesnail.alt = "Personaggio immagine footer";
+personagesnail.src = "./assets/img/snail.gif";
+
+//Definisco le direzione del personaggio caricando due immagini a specchio
 personagesnail.src = "./assets/img/snail_rotate.gif";
 const pathreverse = personagesnail.src;
-
+//Riporto il personaggio alla posizione iniziale
 personagesnail.src = "./assets/img/snail.gif";
 const path1 = personagesnail.src; 
 
 //Variabile attraverso il quale tengo il conteggio del contatore
 let i = 0;
  
-
-
-
-//Inizio funzioni eventListener
+//INIZIO FUNZIONI
 
 //eventLister 'click' sul btnincrement per incrementare il contatore ed attivare l'animazione del personaggio e dello sfondo del footer
-
 incrementbutton.addEventListener("click", ((e) => { 
   
   //Richiamo la funzione animationFooter() che serve a spostare lo sfondo dietro al personaggio
@@ -40,15 +79,12 @@ incrementbutton.addEventListener("click", ((e) => {
  i++;
  displaycounter.textContent = i ;
 
-
 }));
 
 //eventLister 'click' sul btndecrement per decrementare il contatore ed attivare l'animazione del personaggio e dello sfondo del footer a specchio
-
 decrementbutton.addEventListener("click", ((e) => { 
   
-  animationFooter("-"); //Richiamo la funzione animationFooter() passando '-' come valore
-  
+  animationFooter("-"); //Richiamo la funzione animationFooter() passando '-' come valore  
   
   if (personagesnail.src == pathreverse){
     personagesnail.src = "./assets/img/snail_rotate_alt.gif";
@@ -64,6 +100,7 @@ decrementbutton.addEventListener("click", ((e) => {
 
 
 
+//Funzione che permette di spostare lo sfondo del footer in base alla direzione passata come parametro
 function animationFooter (direction) {
 
     //Imposta la quantità di movimento del personaggio.
@@ -72,21 +109,20 @@ function animationFooter (direction) {
     //Apprendo le coordinate del background del footer 
     let footer = document.querySelector('footer');
     let coordinateBackground = window.getComputedStyle(footer).getPropertyValue('background-position');
-    
+  
     //divido le coordinate e creo un array di posizioni per poterci lavorare
     let coordinateArray = coordinateBackground.split(" ");    
     let x = parseInt(coordinateArray[0]);
-    let y = parseInt(coordinateArray[1]);
-    
+    let y = parseInt(coordinateArray[1]);    
  
-    // Controllo della direzione: '+' l'asse x si muovera in maniera negativa, '-'
-    if (direction === "+") {
+    // Controllo della direzione: '+' l'asse x si muovera in maniera negativa, '-' al contrario
+    if (direction == "+") {
         x -= movement;
     } else {
         x += movement;
     }
 
     // Imposta la nuova posizione dello sfondo
-    footer.style.backgroundPosition = `${x}px ${y}px`;
+    footer.style.backgroundPosition = `${x}px ${y}px`;  
 
 };
